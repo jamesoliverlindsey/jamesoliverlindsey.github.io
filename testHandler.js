839f3b930d12ea1de4dd2778e3950b3a8177388b
JamesLindsey <Jamesoliverlindsey@gmail.com>
2017-06-02 16:52:53 -0700

    added button interativity on a test page

---
+++ testHandler.js
@@ -0,0 +1,23 @@
+/**
+ * Created by James on 6/2/2017.
+ */
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+
+
+function msg() {
+
+    var text = document.getElementById("inputText").value;
+
+    alert(text);
+}
