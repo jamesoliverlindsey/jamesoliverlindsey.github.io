101cb9652db1433ac7f4dbe7b380ed882f9c606b
JamesLindsey <Jamesoliverlindsey@gmail.com>
2017-06-02 12:34:20 -0700

    updated button features

---
+++ buttonHandler.js
@@ -0,0 +1,28 @@
+/**
+ * Created by James on 5/23/2017.
+ */
+//https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_button
+
+//https://www.w3schools.com/howto/howto_css_modals.asp
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_bottom
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal2
+
+//https://www.w3schools.com/jsref/dom_obj_button.asp
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+
+
+function msg() {
+
+        alert("Hello! I am an alert box!");
+}
+
